2248555f72259113462b38ff3374671cd2a1a6b7
JMcunst <xnslqjtmghaf@gmail.com>
2024-06-09 00:03:45 +0900

    fix : category interface

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,11 +1,16 @@
-const CategoryComponent = ({ category }) => {
-    return (
-      <div className="text-center mt-5">
-        <h2 className="text-lg font-semibold">{category}</h2>
-        <img src={`/images/home.png`} alt={`${category} Home`} className="mx-auto w-48 h-48" style={{ zIndex: 3, transform: 'translateY(15%)' }}/>
-      </div>
-    );
-  };
-  
-  export default CategoryComponent;
-  
+import React from 'react';
+
+interface CategoryComponentProps {
+  category: string; // category의 타입을 string으로 선언
+}
+
+const CategoryComponent: React.FC<CategoryComponentProps> = ({ category }) => {
+  return (
+    <div className="text-center mt-5">
+      <h2 className="text-lg font-semibold">{category}</h2>
+      <img src={`/images/home.png`} alt={`${category} Home`} className="mx-auto w-48 h-48" style={{ zIndex: 3, transform: 'translateY(15%)' }} />
+    </div>
+  );
+};
+
+export default CategoryComponent;
